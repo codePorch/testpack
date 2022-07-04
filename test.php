5d35f59f8b9300b8438501e761c2c7fb1a4951c8
codePorch <61741969+codePorch@users.noreply.github.com>
2022-07-04 22:49:14 +0400

    Update test.php
    testing

--- test.php
+++ test.php
@@ -8,33 +8,7 @@ $client = new SearchEngine();
 $client->setEngine("google.ae");
 $results = $client->search(["cakes"]); 
 
-$search_parameters=$results['search_parameters']['q'];
 
-$position=1;
-$items = new ArrayIterator();
-foreach ($results['organic_results'] as $number => $result) 
-{
-
-
-$link=(array_key_exists("link", $result)) ? $result['link'] : "";
-$title=(array_key_exists("title", $result)) ? $result['title'] : "";
-$description=(array_key_exists("snippet", $result)) ? $result['snippet'] : "";
-$promoted=$result['type'];
-
-	
-		$res = array(
-			"keyword " => $search_parameters,
-			"ranking " => $position,
-			"url" => $link,
-			"title" => $title,
-			"description "=>$description,
-			"promoted "=>$promoted
-		);
-	
-	
-  $position++;
-  $items->append($res);
-}
 header('Content-Type: application/json');
-print_r(json_encode($items,JSON_PRETTY_PRINT));
+print_r(json_encode($results,JSON_PRETTY_PRINT));
 ?>
